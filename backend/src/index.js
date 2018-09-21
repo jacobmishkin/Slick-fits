522ba13613556aa9f38af8ea447362985524b6da
Jacob Mishkin <user@MacBook-Pro-3.local>
2018-09-21 07:38:28 -0500

    created: backend server setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,22 @@
-// lets go!
+require('dotenv').config({ path: '.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+
+const server = createServer();
+
+
+//  TODO: use Express middleware to handel cookies (JWT)
+// TODO: Use Express middleware to populate current User
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  (deets) => {
+    console.log(`🚀🚀🚀 Server is now running on port http://localhost:${deets.port}`);
+  },
+);
