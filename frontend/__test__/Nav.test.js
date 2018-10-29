b0e71ef098c70e76672cae66ec42abd154464ec1
Jacob Mishkin <user@MacBook-Pro-3.local>
2018-10-29 12:05:03 -0500

    created: test for nav and PleaseSignIn components

---
+++ frontend/__test__/Nav.test.js
@@ -0,0 +1,48 @@
+import { mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import wait from 'waait';
+import { MockedProvider } from 'react-apollo/test-utils';
+import Nav from '../components/Nav';
+import { CURRENT_USER_QUERY } from '../components/User';
+import { fakeUser } from '../lib/testUtils';
+
+const notSignedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: null } },
+  },
+];
+
+const signedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: fakeUser() } },
+  },
+];
+
+describe('<Nav />', () => {
+  it('renders a minimal nav when signed out', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={notSignedInMocks}>
+        <Nav />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    const nav = wrapper.find('ul[data-test="nav"]');
+    expect(nav.children().length).toBe(2);
+    expect(toJSON(nav)).toMatchSnapshot();
+  });
+  it('renders full nav when signed in', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={signedInMocks}>
+        <Nav />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    const nav = wrapper.find('ul[data-test="nav"]');
+    expect(nav.children().length).toBe(5);
+    expect(nav.text()).toContain('Sign Out');
+  });
+});
