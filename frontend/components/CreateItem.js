95a73f36805dddcc7067d41a2781b73e18b026c9
Jacob Mishkin <user@MacBook-Pro-3.local>
2018-10-30 12:43:00 -0500

    created: test mutations for CreateItem component

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -42,7 +42,6 @@ class CreateItem extends Component {
   };
 
   uploadFile = async e => {
-    console.log('uploading file');
     const files = e.target.files;
     const data = new FormData();
     data.append('file', files[0]);
@@ -53,7 +52,6 @@ class CreateItem extends Component {
       body: data,
     });
     const file = await res.json();
-    console.log(file);
     this.setState({
       image: file.secure_url,
       largeImage: file.eager[0].secure_url,
@@ -65,13 +63,13 @@ class CreateItem extends Component {
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
         {(createItem, { loading, error }) => (
           <Form
+            data-test="form"
             onSubmit={async e => {
               //	Stop the form from submitting
               e.preventDefault();
               //	CAll the mutation
               const res = await createItem();
               // Change them to the single Item page
-              // TODO: change for pagination
               Router.push({
                 pathname: '/item',
                 query: { id: res.data.createItem.id },
